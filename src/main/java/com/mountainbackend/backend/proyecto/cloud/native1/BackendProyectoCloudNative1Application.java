4b16f3d20b11e9f94d1d3d0d07b968c3eb6ef9c4
W607-PC01 <bria.aravena@duocuc.cl>
2026-08-26 11:44:01 -0400

    1.0.0 - primer commit

---
+++ src/main/java/com/mountainbackend/backend/proyecto/cloud/native1/BackendProyectoCloudNative1Application.java
@@ -0,0 +1,13 @@
+package com.mountainbackend.backend.proyecto.cloud.native1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProyectoCloudNative1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProyectoCloudNative1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountainbackend/backend/proyecto/cloud/native1/BackendProyectoCloudNative1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountainbackend.backend.proyecto.cloud.native1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProyectoCloudNative1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
